b173c1ddf91993a8055a69b07dee92310cebd874
Mvrshaa <mvrshaa@gmail.com>
2025-05-18 18:18:51 +0200

    Lazy pagination generator

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,10 @@
+def paginate_users(page_size, offset):
+    import seed
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s", (page_size, offset))
+    rows = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    for row in rows:
+        print(row)
--- python-generators-0x00/2-main.py
+++ python-generators-0x00/2-main.py
@@ -1,21 +1,11 @@
-#!/usr/bin/python3
-"""
-Test script for lazy pagination (Task 3)
-"""
 
+"""
+Test script for lazy pagination 
+"""
 import sys
-from 2_lazy_paginate import lazy_paginate
+lazy_paginator = __import__('2-lazy_paginate')
 
-def main():
-    try:
-        # Test pagination with 100 items per page
-        for page in lazy_paginate(100):
-            for user in page:
-                print(user)
-                print()  # Empty line between users
-                
-    except BrokenPipeError:
-        sys.stderr.close()
-
-if __name__ == "__main__":
-    main()
+try:
+    lazy_paginator.paginate_users(100,0)
+except BrokenPipeError:
+    sys.stderr.close()
