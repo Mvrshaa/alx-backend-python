acc9e27925793f153393c60b5576c158e638e9ec
Mvrshaa <mvrshaa@gmail.com>
2025-05-18 18:19:31 +0200

    Memory-efficient age averaging

--- python-generators-0x00/3-main.py
+++ python-generators-0x00/3-main.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python3
 """
-Test script for age calculation (Task 4)
+Test script for age calculation(stream ages) 
 """
 
-from 4_stream_ages import calculate_average_age
+import sys
+stream_ages = __import__('4-stream_ages')
 
-def main():
-    average = calculate_average_age()
-    print(f"Average age of users: {average:.2f}")
-
-if __name__ == "__main__":
-    main()
+try:
+    stream_ages.calculate_average_age()
+except BrokenPipeError:
+    sys.stderr.close()
---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,17 @@
+def stream_user_ages():
+    import seed
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor()
+    cursor.execute("SELECT age FROM user_data")
+    for (age,) in cursor:
+        yield age
+    cursor.close()
+    connection.close()
+
+def calculate_average_age():
+    total = 0
+    count = 0
+    for age in stream_user_ages():
+        total += age
+        count += 1
+    print(f"Average age of users: {total / count:.2f}")
